d12b544c9903897d320f7af881d28db6cf40115a
shayan ghazali <shayan97.tti@gmail.com>
2023-12-13 21:16:56 +0330

    a missed item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { LoginComponent } from './pages/login/login.component';
 export const routes: Routes = [
     { path: 'cars-list', component: CarsListComponent },
     { path: 'login', component: LoginComponent },
-    { path: '', pathMatch: 'full', redirectTo: 'cars-list' },
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
 ];
